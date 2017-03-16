8bb5519ed08b0e53124395f1a2d971dbf9cf255f
Yoda45 <ninjadinosaur.ilikecake@gmail.com>
2017-03-16 17:26:57 +0000

    cenas no program e novo ficheiro

--- program.c
+++ program.c
@@ -1,5 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<strings.h>
 #include "interface.h"
 
+
+
 int main(){
-    return 0;
+    TAD_istruct qs = init();
+    qs = load(qs,argc-1,&argv[1]);
+    printf("total_articles %lu",all_articles(qs));
+
+    printf("unique_articles %lu",unique_articles(qs));
+
+    printf("unique_revisions %lu",all_revisions(qs));
+
+    long *contributors=top_10_contributors(qs);
+
+    printf("top_10_contributors ids: \n");
+
+    for(int i=0; i<10; i++){
+        printf("%lu = %s\n",contributors[i],contributor_name)
+    }
 }
